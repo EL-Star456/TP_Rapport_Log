6cfca24d370f688e6159e4a6eebad0e6e8b84dfc
EL-Star456 <mr.elmelianiammar456@gmail.com>
2026-04-01 21:58:40 +0100

    feat: structure finale - code + rapport PDF par TP

--- monolith/TP2_Monolithe_Modulaire/tp2/src/main/java/com/ecommerce/monolith/product/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.monolith.product.repository;
-
-import com.ecommerce.monolith.product.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
